aa6cee8056010817b17e2517180c776e6e98ef59
Frederick Tetteh <kwesiossom@gmail.com>
2022-11-30 21:07:10 -0500

    tree - bst insert

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  //insert values into bst
+  insert(value) {
+    const newNode = new Node(value);
+    //insert into root if tree is empty
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    // variables to store current node and a bool for indicating if the insertion is done
+    let currentNode = this.root;
+    let insertionCheck = false;
+
+    while (!insertionCheck) {
+      if (newNode.value >= currentNode.value) {
+        if (currentNode.right == null) {
+          currentNode.right = newNode;
+          insertionCheck = true;
+        } else currentNode = currentNode.right;
+      } else {
+        if (currentNode.left == null) {
+          currentNode.left = newNode;
+          insertionCheck = true;
+        } else currentNode = currentNode.left;
+      }
+    }
+  }
+}
+
+const bst1 = new BinarySearchTree();
+bst1.insert(10);
+bst1.insert(4);
+bst1.insert(6);
+bst1.insert(12);
+console.log(bst1);
