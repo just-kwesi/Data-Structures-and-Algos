eb49e9650dca71ad7a596f9b243520f6ba0a5606
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-29 10:57:26 -0600

    basic hash function not ideal

--- hash-table/hash-table.py
+++ hash-table/hash-table.py
@@ -1,1 +1,11 @@
 # defining and working with hash tables
+
+def hash(key,arrayLen):
+    total = 0
+    for c in key:
+      total += ord(c)
+    #   print(total)
+        
+    return total%arrayLen
+
+print(hash("hello",11))
