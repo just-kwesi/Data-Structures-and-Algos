59db642fedb387f5af330ffdbfb4ae061388ce44
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-29 11:05:29 -0600

    used prime to reduce collision

--- hash-table/hash-table.py
+++ hash-table/hash-table.py
@@ -1,11 +1,13 @@
 # defining and working with hash tables
 
+#use a prime number as arrray length
 def hash(key,arrayLen):
     total = 0
+    WIERD_PRIME = 31
     for c in key:
       total += ord(c)
     #   print(total)
         
-    return total%arrayLen
+    return (total*WIERD_PRIME) %arrayLen
 
 print(hash("hello",11))
