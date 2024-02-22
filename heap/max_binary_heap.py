037288bbe1440877b5308fc816dc98c4fc64e342
Frederick Tetteh <kwesiossom@gmail.com>
2024-02-21 18:37:26 -0600

    binary heap - insert using array/list

--- heap/max_binary_heap.py
+++ heap/max_binary_heap.py
@@ -2,5 +2,24 @@ class Max_binary_heap:
     def __init__(self) -> None:
         self.heap = []
     
-    def insert(value):
-        
+    def insert(self,value):
+        self.heap.append(value)
+       
+        idx = len(self.heap) - 1
+        parent_idx = (idx-1)//2
+       
+        while parent_idx >= 0 and (self.heap[parent_idx] < self.heap[idx]):
+           self.heap[parent_idx],self.heap[idx] = self.heap[idx], self.heap[parent_idx]
+           
+           idx = parent_idx
+           parent_idx = (idx-1)//2
+           
+        print(self.heap)
+
+
+heap = Max_binary_heap()
+heap.insert(4)
+heap.insert(7)
+heap.insert(20)
+heap.insert(23)
+heap.insert(2)
