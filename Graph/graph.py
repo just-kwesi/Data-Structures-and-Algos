3f079fbd0b23d51002e5787080cbcc6de2ea06c6
Frederick Tetteh <kwesiossom@gmail.com>
2024-02-21 22:51:31 -0600

    graph: removeEdge, removeVertex

--- Graph/graph.py
+++ Graph/graph.py
@@ -16,10 +16,31 @@ class Graph:
             self.adjacentList[value1].append(value2)
             self.adjacentList[value2].append(value1)
         
-        print(self.adjacentList)
+        # print(self.adjacentList)
         
         return self
-                
+    
+    def removeEdge(self,v1,v2):
+        # print(self.adjacentList)
+        if self.adjacentList.get(v1,None) != None and \
+            self.adjacentList.get(v2,None) != None:
+            self.adjacentList[v1] = [e for e in self.adjacentList[v1] if e != v2]
+            self.adjacentList[v2] = [e for e in self.adjacentList[v2] if e != v1]
+        
+        # print(self.adjacentList)
+        return self
+    
+    def removeVertex(self,v):
+        vertex = self.adjacentList.get(v,None)
+        
+        print(self.adjacentList)
+        if vertex:
+            for edge in vertex:
+                self.adjacentList[edge] = [e for e in self.adjacentList[edge] if e != v]
+            del self.adjacentList[v]
+        print(self.adjacentList)
+        
+        return vertex
 
 
 
@@ -28,3 +49,5 @@ my_graph.addVertex("start")
 my_graph.addVertex("second")
 my_graph.addVertex("friedn")
 my_graph.addVertex("start").addEdge('start','second').addEdge('second','friedn')
+# my_graph.removeEdge('second','friedn')
+my_graph.removeVertex('start')
