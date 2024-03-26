580bdd0fce9ab05c28d8ef86c43907a7705d0673
Frederick Tetteh <kwesiossom@gmail.com>
2024-03-25 21:02:17 -0500

    permutation

--- recursion/combination.py
+++ recursion/combination.py
@@ -18,5 +18,21 @@ def combine(n: int, k: int):
         return res
     
 
+def permute(nums):
+        res = []
 
-combine(4,2)
+        if len(nums) ==1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+        return res
+
+
+permute([1,2,3])
